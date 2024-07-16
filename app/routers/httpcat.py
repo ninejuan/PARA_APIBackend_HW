74540bbc54e69b692f6b6ec0864dfd9f642a12e2
juany <ninejuan@protonmail.com>
2024-07-16 11:49:19 +0000

    add: HTTP CAT

--- app/routers/httpcat.py
+++ app/routers/httpcat.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter, FastAPI, Header, HTTPException
+from fastapi import APIRouter, FastAPI, Header, HTTPException, Response
 from pydantic import BaseModel
 from typing import Optional
+import requests
+
+hCatRouter = APIRouter()
 
 @hCatRouter.get("/")
 async def get_http_cat(code: int = 200):
-    return {"message": "HTTP Cat"}
+    response = requests.get(f'https://http.cat/{code}')
+    if response.status_code == 200:
+        return Response(content=response.content, media_type='image/jpeg')
+    else:
+        return Response(content=f"사진 받기에 실패했습니다. Status code: {response.status_code}", media_type="text/plain")
