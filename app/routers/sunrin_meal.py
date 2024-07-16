0c8bf14234ac047c39b0be02268291e21dea51ea
juany <ninejuan@protonmail.com>
2024-07-16 11:33:07 +0000

    add: SRI Meal Data

--- app/routers/sunrin_meal.py
+++ app/routers/sunrin_meal.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter, FastAPI, Header, HTTPException
 from pydantic import BaseModel
 from typing import Optional
-import database
+import database, requests
 
 db = database.db
 sriMealRouter = APIRouter()
 
+# Sunrin Meal NPI EndPoint
+# https://npi.ny64.kr/snt_lunch?month=<월>&day=<일>
+
 @sriMealRouter.get("/")
-async def get_sunrin_meal():
-    return {"message": "Sunrin Meal"}
+async def get_sunrin_meal(month: int = 3, day: int = 5):  
+    response = requests.get(f"https://npi.ny64.kr/snt_lunch?month={month}&day={day}").json()
+    meals = response["data"][0]
+    return {
+        "code": 200,
+        "date": meals['date'],
+        "meals": meals["menu"]
+    }
